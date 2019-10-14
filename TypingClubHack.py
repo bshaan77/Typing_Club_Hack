16bca2ea106dad77effa16d58e96271aa3157eff
IshaanBansal <ishaanbansal1001@gmail.com>
2019-10-13 20:54:06 -0700

    Update TypingClubHack.py

--- TypingClubHack.py
+++ TypingClubHack.py
@@ -7,29 +7,39 @@ keyboard = Controller()
 #delay = 0.11231 * pow(2, 0.092854 * random.randint(0, 101))
 #delay = 0.009 * pow(2, 0.062854 * 55)
 
-
 def error (errorNum):
-    errorList = ("fksdlajf89quyr8934hqw9ufhe9u8ahe98h2fqp9hadujbdsvycayurwsgdfcvuyfgeuyg8923hp09h))(#D@&^hbeuiwfhb9uiebwqifrbfuejsdkfndas.dsa'asd'f[as]\fafiuhegfiqwugfu8vgsauf")
-    for i in errorList:
-        errorChar = i
+    errorList = ("qwertyuiopasdfghjklzxcvbnm,;'qwertyuiopzxcvbnm;'fksdlajf89quyr8934hqw9ufhe9u8ahe98h2fqp9hadujbdsvycayurwsgdfcvuyfgeuyg8923hp09h))(#D@&^hbeuiwfhb9uiebwqifrbfuejsdkfndas.dsa'asd'f[as]\fafiuhegfiqwugfu8vgsauf")
+    errorChar = errorList[errorNum]
+    #for i in errorList:
+        #errorChar = i
     #errorChar = errorNum in errorList
     return errorChar
 
 def controlKeyboard():
-    AccuracyControl = random.randint(90,97)
-    for char in '''At first, Washington D.C. was made up of a piece of Virginia located south of the Potomac River and a piece of Maryland located north of the Potomac River. In 1847, Virginia's land was returned to it. This area is now part of Arlington County. Since 1847, all of Washington D.C. is on the north side of the Potomac River.''':
+    AccuracyControl = random.randint(83,95)
+    for char in '''The judicial branch of the U.S. government is made up of the court system. The Supreme Court is the highest court in the country. The 9 justices are nominated by the president and must be approved by the Senate (with at least 51 votes). There are lower Federal courts that were created by Congress using power granted from the Constitution.''':
         errorFactor = random.randint(0,100)
         if errorFactor <= AccuracyControl:
             keyboard.press(char)
             keyboard.release(char)
-        else:
-            errorChar = error(random.randint(0,60))
+        elif errorFactor <= (AccuracyControl+5):
+            errorChar = error(random.randint(0,100))
+            print(errorChar)
             keyboard.press(errorChar)
             keyboard.release(errorChar)
-        delay = random.uniform(0.07,0.15)
+            time.sleep(float(0.3))
+            keyboard.press(Key.backspace)
+            time.sleep(float(0.2))
+            keyboard.press(char)
+            keyboard.release(char)
+        else:
+            errorChar = error(random.randint(0,100))
+            keyboard.press(errorChar)
+            keyboard.release(errorChar)
+        delay = random.uniform(0.06,0.13)
         time.sleep(float(delay))
-        print(char)
-        print(delay)
+        #print(char)
+        #print(delay)
 
-sleep(3); controlKeyboard()
+sleep(5); controlKeyboard()
 
